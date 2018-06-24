8bd513a05b037705e63ce6dafbdd3ee50d383645
Abhinav Krishnamoorthy <abhinav.krishnamoorthy@gmail.com>
2018-06-24 15:04:17 -0400

    Changes for chapter 6

--- chapter5/app.js
+++ chapter5/app.js
@@ -1,1 +1,29 @@
-console.log('It works');
+let express = require('express');
+let app = express();
+
+let path = require('path');
+
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/json', (req, res) => {
+  console.log('Get the json');
+  res
+    .status(200)
+    .json({
+      'jsonData': true
+    });
+});
+
+app.get('/file', (req, res) => {
+  console.log('Get the file');
+  res
+    .status(200)
+    .sendFile(path.join(__dirname,'app.js'));
+});
+
+let server = app.listen(app.get('port'), () => {
+  let port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});

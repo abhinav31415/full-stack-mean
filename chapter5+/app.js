9301a72a9598ae542b3e097143df520da5da1aa1
Abhinav Krishnamoorthy <abhinav.krishnamoorthy@gmail.com>
2018-06-24 17:46:14 -0400

    Deleting code from app.js

--- chapter5+/app.js
+++ chapter5+/app.js
@@ -15,22 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
 
-app.get('/json', (req, res) => {
-  console.log('Get the json');
-  res
-    .status(200)
-    .json({
-      'jsonData': true
-    });
-});
-
-app.get('/file', (req, res) => {
-  console.log('Get the file');
-  res
-    .status(200)
-    .sendFile(path.join(__dirname,'app.js'));
-});
-
 let server = app.listen(app.get('port'), () => {
   let port = server.address().port;
   console.log('Magic happens on port ' + port);
